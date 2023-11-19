d6dc8bbd73cc7c1fb28e75d4d9df40cc7a475694
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-11-19 12:27:07 -0400

    sign in / sign up form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,10 @@
+import { LoginForm } from "../components/LoginForm";
 
 const Login = () => {
   return (
     <div>
       <h1>Esta es la vista de Login</h1>
+      <LoginForm />
     </div>
   );
 };
