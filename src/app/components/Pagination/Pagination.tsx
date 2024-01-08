a63f18305b0b69b4cbaca3baccea538a0453010c
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-01-08 19:33:23 -0400

    paginated 9/10

--- src/app/components/Pagination/Pagination.tsx
+++ src/app/components/Pagination/Pagination.tsx
@@ -15,18 +15,18 @@ const Pagination = ({
 }: Pagination) => {
   const pageNumbers: number[] = [];
 
-  
   for (let i = 1; i <= Math.ceil(products.length / pageSize); i++) {
     pageNumbers.push(i);
   }
 
   console.log("Estos son los números de página: ", pageNumbers);
+  console.log("Esta es la página actual: ", currentPage);
   return (
-    <nav>
-      <ul className="list-style-none flex">
+    <nav className="flex flex-row items-center -space-x-px h-10 mb-8 mt-2">
+      <ul className="flex items-center -space-x-px h-10 text-base">
         <li>
           <button
-            className="pointer-events-none relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-500 transition-all duration-300 dark:text-neutral-400"
+            className="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
             onClick={() => pagination(currentPage - 1)}
             disabled={currentPage === 1}
           >
@@ -37,9 +37,9 @@ const Pagination = ({
         {pageNumbers?.map((number) => (
           <li key={number}>
             <button
-              className={`relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100  dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white ${
+              className={`relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-neutral-100  dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white ${
                 currentPage === number
-                  ? " text-gray-900 focus:bg-red-600 focus-within:bg-red-600 dark:focus:bg-red-700 dark:focus-within:bg-red-700"
+                  ? " text-gray-900 focus:bg-gray-300 focus-within:bg-gray-300 dark:focus:bg-gray-100 dark:focus-within:bg-gray-100"
                   : ""
               }`}
               key={number}
@@ -51,7 +51,7 @@ const Pagination = ({
         ))}
         <li>
           <button
-            className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+            className="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
             onClick={() => pagination(currentPage + 1)}
             disabled={currentPage === pageNumbers.length}
           >
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,24 +5,22 @@ import Card from "../components/Card/Card";
 import { Product } from "../api/products/models";
 import { Pagination } from "../components/Pagination";
 
-//PARA SSR creamos componente de server async/ await y ejecutamos la función 
+//PARA SSR creamos componente de server async/ await y ejecutamos la función
 // async function fetchProducts() {
 //   return await getProducts(); //EJECUTAMOS EL FETCH DE SERVICES
 // }
 const Products = () => {
-  
-  const[products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(6);
 
-useEffect(()=>{
-  async function fetchProducts() {
-    const productsData = await getProducts();
-    setProducts(productsData);
-    
-  };
-  fetchProducts();
-}, []);
+  useEffect(() => {
+    async function fetchProducts() {
+      const productsData = await getProducts();
+      setProducts(productsData);
+    }
+    fetchProducts();
+  }, []);
 
   const indexOfLastProd = currentPage * pageSize;
   const indexOfFirstProd = indexOfLastProd - pageSize;
@@ -33,21 +31,21 @@ useEffect(()=>{
   };
 
   return (
-    <main>
-      <nav>
-    <Pagination pageSize={pageSize}
-    products={products}
-    currentPage={currentPage}
-    pagination={pagination} />
-</nav>
-<section className="grid lg:grid-cols-3 lg:grid-rows-3 lg:gap-6 md:grid-cols-2 my-6 p-6 md:grid-rows-4 md:gap-2 place-items-center">
-  {currentProducts.map((product) => (
-    <Card key={product.id} data={product} />
-  ))}
-</section>
+    <main className="flex flex-col items-center">
+      <Pagination
+        pageSize={pageSize}
+        products={products}
+        currentPage={currentPage}
+        pagination={pagination}
+      />
+
+      <section className="grid lg:grid-cols-3 lg:grid-rows-3 lg:gap-6 md:grid-cols-2 my-6 p-6 md:grid-rows-4 md:gap-2 place-items-center">
+        {currentProducts.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
+      </section>
     </main>
-    
   );
-}
+};
 
 export default Products;
